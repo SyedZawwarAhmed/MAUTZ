f9521f2aefa857aaefd61c0e23560af6a97d8601
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-05-17 06:00:17 +0500

    update checkLoop.py

--- modules/checkLoop.py
+++ modules/checkLoop.py
@@ -9,33 +9,67 @@ def dec(tokens):
 
 def cond(tokens):
     comp_opt = ['==', '<',">",">=", "<=","!="]
-    exp1 = []
-    for i in range(len(tokens)):
-       if tokens[i] not in comp_opt:
-           exp1.append(tokens[i])
-       else:
-           opt = tokens[i]
-           exp2 = tokens[i+1:]
+    log_opt = ['aur', 'nhi', 'ya']
+    exps = []
+    used_operators=[]
+    k = 0
+    n_exp = []
+    while (k < len(tokens)):
+        if tokens[k] not in log_opt:
+           n_exp.append(tokens[k])
+        if tokens[k] in log_opt:
+            used_operators.append(tokens[k])
+            exps.append(n_exp)
+            n_exp=[]
+        k+=1
+    exps.append(n_exp)
+    print(exps)
 
-    return exp(exp1) and opt != '' and exp(exp2)
+    for i in range(len(exps)):
+        for j in range(len(exps[i])):
+            if exps[i][len(exps[i])-1] not in comp_opt and not isValidOperator(exps[i][len(exps[i])-1]):
+                if exps[i][j] in comp_opt:
+                    if(not exps[i][j+1].isnumeric() and not isValidVariable(exps[i][j+1])):
+                        return False
+                elif j%2 == 0:
+                    if(not isValidVariable(exps[i][j]) and not exps[i][j].isnumeric()):
+                        return False
+                else:
+                    if(exps[i][j] not in comp_opt and not isValidOperator(exps[i][j])):
+                        return False
+            if exps[i][len(exps[i])-1] in comp_opt or isValidOperator(exps[i][len(exps[i])-1]):
+                return False
+                
+    return True
 
-def exp(tokens):
+def isValidVariable(token):
+    pattern = r'^[a-zA-Z][a-zA-Z0-9]{0,18}$'
+    return bool(re.match(pattern, token))
+
+def isValidOperator(token):
     operators = ['+', '-', '*', '/', '%', '^']
-    lo
-    if len(tokens) == 1:
-        # check if the var is assigned and return that var
-    
+    if token in operators:
+        return True
 
 def initializer(token):
-    return token == 'jab tak'
+    return token[0] == 'jab' and token[1] == 'tak'
+    
 
 def colon(token):
-    return token == ';'
-print(tokenize('jab tak num1 + num2 <= 10 aur num1 == 1 :'))
+    return token == ":"
 
-# <loop> → jab tak <cond> : <body>
-# <cond> → <exp> <cmp_op> <exp> <NEW> | nahi var1 <NEW>
-# <NEW> → ya <cond> | aur <cond> |Ω
-# <exp> → var1<NEXT>
-# <NEXT> → <mth_op>var2 | Ω
-# < cmp_op> → ==, <,>,>=, <=, 
+print(tokenize('jab tak num1 <= 10 :'))
+print(check_loop_declaration(tokenize('jab tak num1 <= 10 :')))
+
+print(tokenize('jab tak num1 <= 10 + :'))
+print(check_loop_declaration(tokenize('jab tak num1 <= 10 + :')))
+
+print(tokenize('jab tak num1 + num2 <= 10 aur num1 == 1 :'))
+print(check_loop_declaration(tokenize('jab tak num1 + num2 <= 10 aur num1 == 1 :')))
+
+print(tokenize('jab tak num1 + 2num2 <= 10 aur num1 == 1 :'))
+print(check_loop_declaration(tokenize('jab tak num1 + 2num2 <= 10 aur num1 == 1 :')))
+
+print(tokenize('jab tak num1 + num2 <= 10 + 2 aur num1 == 1 ya num2 == 3 :'))
+print(check_loop_declaration(tokenize('jab tak num1 + num2 <= 10 + 2 aur num1 == 1 ya num2 == 3 :')))
+
